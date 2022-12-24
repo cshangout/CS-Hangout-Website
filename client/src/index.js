ca60e7ba44883454b9e93ca16a92c0572aeed599
Nicholas Truong <magatsuken@gmail.com>
2022-12-23 20:17:37 -0500

    Remove App component, add Login component
    Remove App component to add Login component

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './Login/Login';
 
 // Bootstrap import
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -10,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
