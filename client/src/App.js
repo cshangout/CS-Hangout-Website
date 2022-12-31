23766fb1ce0c7397ccae904f3c79e57ead320c4c
ndohertyjr <ndohertyjr@gmail.com>
2022-12-31 13:56:42 -0500

    Added test file for Auth Landing Page component

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,14 @@ import AuthLandingPage from "./Authentication/AuthLandingPage";
 
 // Style imports
 import './App.css';
+import {Container} from "react-bootstrap";
 export default function App() {
   return (
-    <div className="App-Body-Layout">
-      <Header />
-      <AuthLandingPage />
-    </div>
+      <div className="App-Body-Layout">
+          <Container fluid>
+              <Header />
+              <AuthLandingPage />
+          </Container>
+      </div>
   )
 }
