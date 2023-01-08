b73dc21c49c2692b1327b83dafabd84bc5a5c8b7
ndohertyjr <ndohertyjr@gmail.com>
2023-01-08 16:51:58 -0500

    Create startup services for API and registered structured logging

---
+++ server/Server.Tests/API/Controllers/AuthControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using Server.API.Controllers;
+
+namespace Server.Tests.API.Controllers;
+
+public class AuthControllerTests
+{
+    private Mock<ILogger> mockLogger;
+    
+    public AuthControllerTests()
+    {
+        mockLogger = new Mock<ILogger>();
+    }
+
+    [Fact]
+    public void AuthController_Constructor_Returns_Instance_Of_AuthController()
+    {
+        var testController = new AuthController(mockLogger.Object);
+
+        testController.Should().BeOfType<AuthController>();
+    } 
+}
