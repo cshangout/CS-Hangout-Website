152c3cf12ab1c6a6aedfac8d8abb8844e4d9c8df
Nicholas Truong <magatsuken@gmail.com>
2023-01-08 22:13:39 -0500

    Add validation, error handling separate

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.js'
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
@@ -17,8 +17,12 @@ export default function Login() {
     const [show, setShow] = useState(false);
     const [color, setColor] = useState(null);
     const [message, setMessage] = useState(null);
-    const [inputEmail, setEmail] = useState('');
-    const [inputPassword, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [loginInfoValid, setLoginInfoValid] = useState(false);
+    const [usernameValid, setUsernameValid] = useState(false);
+    const [passwordValid, setPasswordValid] = useState(false);
 
     const handleSubmit = (e) => {
         const form = e.currentTarget;
@@ -30,91 +34,133 @@ export default function Login() {
         setValidated(true);
     }
 
-    const handleErrors = (e) => {
-        const username = inputEmail;
-        const password = inputPassword;
+    const onEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
 
-        if (username === '' && password) {
+    const onPasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const validateEmailInput = () => {
+        if (email.length < 40 && email.includes('@')) {
+            setUsernameValid(true);
+        }
+        else {
+            setUsernameValid(false);
+        }
+    }
+
+    const validatePasswordInput = () => {
+        if (password.length >= 8 && password.length < 24) {
+            setPasswordValid(true);
+        }
+        else {
+            setPasswordValid(false);
+        }
+    }
+
+    const isFormValid = () => {
+        if (usernameValid && passwordValid) {
+            setLoginInfoValid(true);
+        }
+        else {
+            setLoginInfoValid(false);
+        }
+    }
+
+    useEffect(() => {
+        validateEmailInput()
+        validatePasswordInput()
+        isFormValid()
+    }, [usernameValid, passwordValid, email, password])
+
+    const handleErrors = () => {
+        if (email === '') {
             setColor(ToastConstants.color.error);
             setMessage(ToastConstants.loginErrorMsg.usernameEmpty);
             setShow(true);
         }
-        if (password === '' && username) {
+        else if (password === '') {
             setColor(ToastConstants.color.error);
             setMessage(ToastConstants.loginErrorMsg.passwordEmpty);
-            setShow(true);      
+            setShow(true);
         }
-        if (password === '' && username === ''){
+        else if (password === '' && email === '') {
             setColor(ToastConstants.color.error);
             setMessage(ToastConstants.loginErrorMsg.bothFieldsEmpty);
-            setShow(true);      
+            setShow(true);
         }
-        if (password && username) {
+        else if (password && email) {
             setColor(ToastConstants.color.error);
-            setMessage(ToastConstants.loginErrorMsg.unauthorizedLogin);  
+            setMessage(ToastConstants.loginErrorMsg.unauthorizedLogin);
             setShow(true);
         }
     }
 
     return (
-    <div>
-        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide={true} color={color} message={message}/>
-        <Container fluid>
-            <Row>
-                <Col />
-                <Col>
-                    <CardGroup>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Body>
-                                <Form
-                                    noValidate
-                                    validated={validated}
-                                    onSubmit={handleSubmit}
-                                    data-testid='login-form-validity-test'>
-                                    {/* FIXME: Change hardcoded code: placeholder, maxLength */}
-                                    <Form.Group className='mb-3'>
-                                        <Form.Label htmlFor='email'>Email Address</Form.Label>
-                                        <Form.Control
-                                            type='email'
-                                            placeholder={LoginConstants.email.placeholder}
-                                            maxLength={40} id='email'
-                                            required data-testid='login-form-email-input' 
-                                            onChange={(e) => setEmail(e.target.value)}/>
-                                        <Form.Control.Feedback
-                                            type='invalid'
-                                            data-testid='email-err-msg'
-                                        >Please enter a valid email.
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-                                    {/* FIXME: Change hardcoded code: placeholder, minLength, maxLength */}
-                                    <Form.Group className='mb-3'>
-                                        <Form.Label htmlFor='password'>Password</Form.Label>
-                                        <Form.Control
-                                            type='password'
-                                            placeholder='Enter Password'
-                                            minLength={LoginConstants.password.minLength}
-                                            maxLength={LoginConstants.password.maxLength}
-                                            id='password'
-                                            required
-                                            data-testid='login-form-password-input' 
-                                            onChange={(e) => setPassword(e.target.value)}/>
-                                        <Form.Control.Feedback
-                                            type='invalid'
-                                            data-testid='password-err-msg'
-                                        >Please enter a password with a minimum of 8 chars.
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-                                    <Button onClick={handleErrors} variant="primary" type="submit">
-                                        Login
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </CardGroup>
-                </Col>
-                <Col />
-            </Row>
-        </Container>
-    </div>
+        <div>
+            {show ? <Toast
+                onClose={() => setShow(false)}
+                show={show}
+                delay={3000}
+                autohide={true}
+                color={color}
+                message={message} /> : null}
+            <Container fluid>
+                <Row>
+                    <Col />
+                    <Col>
+                        <CardGroup>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Form
+                                        noValidate
+                                        validated={validated}
+                                        onSubmit={handleSubmit}
+                                        data-testid='login-form-validity-test'>
+                                        <Form.Group className='mb-3'>
+                                            <Form.Label htmlFor='email'>Email Address</Form.Label>
+                                            <Form.Control
+                                                type='email'
+                                                placeholder={LoginConstants.email.placeholder}
+                                                maxLength={40} id='email'
+                                                required data-testid='login-form-email-input'
+                                                onChange={(e) => onEmailChange(e)} />
+                                            <Form.Control.Feedback
+                                                type='invalid'
+                                                data-testid='email-err-msg'
+                                            >Please enter a valid email.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group className='mb-3'>
+                                            <Form.Label htmlFor='password'>Password</Form.Label>
+                                            <Form.Control
+                                                type='password'
+                                                placeholder='Enter Password'
+                                                minLength={LoginConstants.password.minLength}
+                                                maxLength={LoginConstants.password.maxLength}
+                                                id='password'
+                                                required
+                                                data-testid='login-form-password-input'
+                                                onChange={(e) => onPasswordChange(e)} />
+                                            <Form.Control.Feedback
+                                                type='invalid'
+                                                data-testid='password-err-msg'
+                                            >Please enter a password with a minimum of 8 chars.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Button onClick={handleErrors} variant="primary" type="submit" disabled={!loginInfoValid}>
+                                            Login
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        </CardGroup>
+                    </Col>
+                    <Col />
+                </Row>
+            </Container>
+        </div>
     )
 }
