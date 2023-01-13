0d45aaf9b9792832ce0264a67d219e0af2c73d5f
Garrett <garrettgibson0530@gmail.com>
2023-01-12 23:30:13 -0500

    header with state functionality

---
+++ client/src/Button/LoginButton.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+
+function LoginButton() {
+    return (
+        <>
+
+            <Button variant="outline-primary">Login</Button>
+
+        </>
+    );
+}
+
+export default LoginButton;
