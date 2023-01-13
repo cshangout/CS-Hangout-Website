56146ff6724d98dcd404a6c6a263f19bbdc3ea31
Garrett <garrettgibson0530@gmail.com>
2023-01-12 23:30:13 -0500

    header with state functionality

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,7 +1,33 @@
-
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Col} from 'react-bootstrap';
+import LoginButton from '../Button/LoginButton';
+import {useState} from "react";
+import Button from "react-bootstrap/Button";
 export default function Header() {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
     return (
-        <h1>Header</h1>
-    )
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">CS Hangout</Navbar.Brand>
+                <Navbar.Collapse className="justify-content-end">
+                    {loggedIn ?
+                            <Nav>
+                                <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
+                                <Button variant="outline-primary">Log out</Button>
+                            </Nav>
+                         :
+                            <Nav>
+                                <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
+                                <LoginButton/>
+                            </Nav>
+                    }
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
+
