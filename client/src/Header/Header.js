a54ac0c2349a6205c178e1296873acb64ab9b047
ndohertyjr <ndohertyjr@gmail.com>
2023-01-15 00:01:48 -0500

    Removed unused code

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,10 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Col} from 'react-bootstrap';
-import LoginButton from '../Button/LoginButton';
 import {useState} from "react";
 import Button from "react-bootstrap/Button";
+
 export default function Header() {
 
     const [loggedIn, setLoggedIn] = useState(false);
@@ -15,15 +14,15 @@ export default function Header() {
                 <Navbar.Brand href="#home">CS Hangout</Navbar.Brand>
                 <Navbar.Collapse className="justify-content-end">
                     {loggedIn ?
-                            <Nav>
-                                <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
-                                <Button variant="outline-primary">Log out</Button>
-                            </Nav>
+                        <Nav>
+                            <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
+                            <Button variant="outline-primary">Log out</Button>
+                        </Nav>
                          :
-                            <Nav>
-                                <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
-                                <LoginButton/>
-                            </Nav>
+                        <Nav>
+                            <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
+                            <Button variant="outline-primary">Login</Button>
+                        </Nav>
                     }
                 </Navbar.Collapse>
             </Container>
