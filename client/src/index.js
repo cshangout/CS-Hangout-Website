782b0ab384a02c07654743450ddfb3f16a132409
Garrett <garrettgibson0530@gmail.com>
2023-01-15 21:01:20 -0500

    browser router that directs to app main page

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Bootstrap import
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter, RouterProvider
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App /> ,
+    }, {
+        path: "/butts",
+        element: <div> <h1>testicles</h1> </div>,
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <RouterProvider router={router} />
+
 );
 
 // If you want to start measuring performance in your app, pass a function
