598283a3287cd0da556a49167a33e33363b402f9
ndohertyjr <ndohertyjr@gmail.com>
2023-01-16 10:31:01 -0500

    Updated auth tests

---
+++ server/Server.Tests/TestData/Dtos/TestRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using Server.API.DTOs;
+
+namespace Server.Tests.TestData.Dtos;
+
+public static class TestRegisterDto
+{
+    public static RegisterDto GetRegisterDto(
+        string username = "testUser",
+        string email = "testemail@gmail.com",
+        string password = "123456789")
+    {
+        return new RegisterDto()
+        {
+            UserName = username,
+            Email = email,
+            Password = password
+        };
+    }
+}
