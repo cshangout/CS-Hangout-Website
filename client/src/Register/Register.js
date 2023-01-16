080335eeaf31193040c1294e31ef699ed23a7a86
Nicholas Truong <magatsuken@gmail.com>
2023-01-16 15:24:01 -0500

    Add API call to backend and error handling

--- client/src/Register/Register.js
+++ client/src/Register/Register.js
@@ -1,5 +1,5 @@
 import { Container } from "react-bootstrap"
-import { UserConstants } from "../__helpers__/Constants"
+import { UserConstants, ToastConstants } from "../__helpers__/Constants"
 import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -7,9 +7,13 @@ import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Toast from '../Toast/Toast';
 
 // FIXME: Review formating of extra columns
 export default function Register() {
+    const [show, setShow] = useState(false);
+    const [color, setColor] = useState(null);
+    const [message, setMessage] = useState(null);
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -19,6 +23,34 @@ export default function Register() {
     const [emailValid, setEmailValid] = useState(false);
     const [usernameValid, setUsernameValid] = useState(false);
     const [passwordValid, setPasswordValid] = useState(false);
+
+    const handleSubmit = async (e) => {
+        const form = e.currentTarget;
+        e.preventDefault();
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                Username: username,
+                Email: email,
+                Password: password
+            })
+        };
+
+        fetch('https://localhost:5001/api/auth/register', requestOptions)
+            .then(function (response) {
+                console.log("Status Code: " + response.status);
+                if (!response.ok) {
+                    handleErrors(response.status);
+                }
+                else {
+                    console.log("Submission successful!");
+                    return response.json();
+                }
+            })
+            .then(data => console.log(data));
+    }
 
     const onEmailChange = (e) => {
         setEmail(e.target.value);
@@ -79,8 +111,40 @@ export default function Register() {
         isFormValid()
     }, [usernameValid, emailValid, passwordValid, username, email, password, checkPassword])
 
+    const handleErrors = (responseCode) => {
+        switch (responseCode) {
+            case 400:
+                setColor(ToastConstants.color.error);
+                setMessage(ToastConstants.registerMsg.userAlreadyRegistered);
+                setShow(true);
+                break;
+            case 401:
+                setColor(ToastConstants.color.error);
+                setMessage(ToastConstants.registerMsg.unauthorizedRegister);
+                setShow(true);
+                break;
+            case 500:
+                setColor(ToastConstants.color.error);
+                setMessage(ToastConstants.registerMsg.contactSupport);
+                setShow(true);
+                break;
+            default:
+                setColor(ToastConstants.color.error);
+                setMessage("Unhandled error code");
+                setShow(true);
+                break;
+        }
+    }
+
     return (
         <div>
+            {show ? <Toast
+                onClose={() => setShow(false)}
+                show={show}
+                delay={3000}
+                autohide={true}
+                color={color}
+                message={message} /> : null}
             <Container fluid className="p-5">
                 <Row>
                     <Col />
@@ -91,6 +155,7 @@ export default function Register() {
                                     <Card.Title className="text-center">Register Form</Card.Title>
                                     <Form
                                         noValidate
+                                        onSubmit={handleSubmit}
                                         data-testid='register-form-validity-test'>
                                         <Form.Group className='mb-3'>
                                             <Form.Label htmlFor='username'>Username</Form.Label>
