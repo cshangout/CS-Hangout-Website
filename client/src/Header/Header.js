da0948e8fa6dd5444f606743d805f1204aaa9c30
MrGibz <66959283+MrGibz@users.noreply.github.com>
2023-01-17 22:13:37 -0500

    added register button if user is not logged in. set up route in index.js for /register and added routing functionality to register button

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -3,24 +3,36 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {useState} from "react";
 import Button from "react-bootstrap/Button";
+import {useNavigate} from "react-router-dom";
 
 export default function Header() {
 
     const [loggedIn, setLoggedIn] = useState(false);
+
+    let navigate = useNavigate();
+    //callable constant for onClick function to direct user to register route
+    const registerRoute = () =>{
+        let path = '/register';
+        navigate(path);
+
+    }
 
     return (
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="#home">CS Hangout</Navbar.Brand>
                 <Navbar.Collapse className="justify-content-end">
+                    {/*if logged in, following displays in header*/}
                     {loggedIn ?
                         <Nav>
                             <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
                             <Button variant="outline-primary">Log out</Button>
                         </Nav>
+                         // if not logged in, this displays
                          :
                         <Nav>
                             <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
+                            <Button variant="outline-primary" onClick={registerRoute}>Register</Button>
                             <Button variant="outline-primary">Login</Button>
                         </Nav>
                     }
