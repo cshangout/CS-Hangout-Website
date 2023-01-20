ed1404bcb8821883b70cad5a548511cb9ec5c81b
ndohertyjr <ndohertyjr@gmail.com>
2023-01-19 23:31:03 -0500

    Fixed whitespace

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -14,7 +14,6 @@ export default function Header() {
     const registerRoute = () =>{
         let path = '/register';
         navigate(path);
-
     }
 
     return (
--- client/src/index.js
+++ client/src/index.js
@@ -24,9 +24,7 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <RouterProvider router={router} />
-
 );
 
 // If you want to start measuring performance in your app, pass a function
