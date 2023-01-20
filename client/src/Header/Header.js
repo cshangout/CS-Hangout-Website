c8a695aa5343483d3d85bd2bafaee9c68f09d309
Nicholas Truong <magatsuken@gmail.com>
2023-01-19 22:03:08 -0500

    Lift state up to use isLoggedIn state

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,27 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {useState} from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 
-export default function Header() {
-
-    const [loggedIn, setLoggedIn] = useState(false);
+export default function Header({ isLoggedIn, setIsLoggedIn }) {
+    const onClick = () => {
+        setIsLoggedIn(isLoggedIn = false);
+    }
 
     return (
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="#home">CS Hangout</Navbar.Brand>
                 <Navbar.Collapse className="justify-content-end">
-                    {loggedIn ?
+                    {isLoggedIn ?
                         <Nav>
                             <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
-                            <Button variant="outline-primary">Log out</Button>
+                            <Button onClick={onClick} data-testid="header-logout-button-test" variant="outline-primary">Log out</Button>
                         </Nav>
-                         :
+                        :
                         <Nav>
                             <Nav.Link href="#home">I am a link! Click me!</Nav.Link>
-                            <Button variant="outline-primary">Login</Button>
+                            <Button variant="outline-primary" data-testid="header-login-button-test" >Login</Button>
                         </Nav>
                     }
                 </Navbar.Collapse>
