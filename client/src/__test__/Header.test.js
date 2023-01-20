8f3aaf129aa2dd6f6b5a725cf80a76086daab897
Nicholas Truong <magatsuken@gmail.com>
2023-01-20 00:05:35 -0500

    Add TODO to test

--- client/src/__test__/Header.test.js
+++ client/src/__test__/Header.test.js
@@ -9,6 +9,7 @@ describe('Test the Header component', () => {
         expect(component).toBeInstanceOf(Object)
     })
 
+    // TODO: useNavigate() may be used only in the context of a <Router> component.
     test('when not logged in, login button shows', () => {
         render(<Header />);
         const loginButton = screen.getByTestId('header-login-button-test');
@@ -16,6 +17,7 @@ describe('Test the Header component', () => {
         expect(loginButton).toBeInTheDocument();
     })
 
+    // TODO: useNavigate() may be used only in the context of a <Router> component.
     test('when logged in, logout button shows', () => {
         render(<Header isLoggedIn={true} />);
 
