752a3c37d845c08495382a68692bcb8b6211afe9
Nicholas Truong <magatsuken@gmail.com>
2023-01-19 22:01:41 -0500

    Create Header.test.js

---
+++ client/src/__test__/Header.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import Header from "../Header/Header";
+
+describe('Test the Header component', () => {
+    test('render Header component', () => {
+        const component = render(<Header />);
+        expect(component).toBeInstanceOf(Object)
+    })
+
+    test('when not logged in, login button shows', () => {
+        render(<Header />);
+        const loginButton = screen.getByTestId('header-login-button-test');
+
+        expect(loginButton).toBeInTheDocument();
+    })
+
+    test('when logged in, logout button shows', () => {
+        render(<Header isLoggedIn={true} />);
+
+        const logoutButton = screen.getByTestId('header-logout-button-test');
+        expect(logoutButton).toBeInTheDocument();
+    })
+})
